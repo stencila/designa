442ed97f822e7253057e5bc03a2bcac234a89ff2
Nokome Bentley <nokome@stenci.la>
2018-11-20 11:51:34 +1300

    Refactor button and heading stories to use notes

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,12 +1,33 @@
 import { storiesOf } from '@storybook/html'
+import { withNotes } from '@storybook/addon-notes'
 
 storiesOf('Atoms/Button', module)
+  .addDecorator(withNotes)
+
   .add('Simple', () => `
     <button class="button">Click me</button>
   `)
+
   .add('Primary', () => `
     <button class="button is-primary">OK</button>
   `)
+
+  .add('Call-to-action', () => `
+    <a class="button call-to-action is-primary is-rounded" href="#">
+      Call to action
+    </a>
+  `, {
+    notes: `A call to action button is a regular button, with additional classes of: <code>.button.call-to-action.is-primary.is-rounded</code>`
+  })
+
+  .add('Call-to-action Large', () => `
+    <a class="button call-to-action is-primary is-rounded" href="#">
+      Large call to action
+    </a>
+  `, {
+    notes: `A call to action button with additional classes of <code>is-large</code>`
+  })
+
   .add('Danger', () => `
     <button class="button is-danger">Delete</button>
-  `,)
+  `)
--- stories/heading.stories.js
+++ stories/heading.stories.js
@@ -1,18 +1,19 @@
 import { storiesOf } from '@storybook/html'
+import { withNotes } from '@storybook/addon-notes'
 
 storiesOf('Layout', module)
+  .addDecorator(withNotes)
+
   .add('Heading', () => `
-    <div class="container">
-        <h1 class="is-size-1 has-text-weight-bold">Heading</h1>
+<div class="heading">
+  <span class="small">What is Stencila?</span>
+  <h1 class="title">An open source office suite for reproducible research.</h1>
+</div>
+  `, {
+    notes: { markdown: `
+## Usage
+A heading is an introduction to a section. 
 
-        <p>A heading is an introduction to a section. It contains an optional small text, h1-h6 tag &amp; an optional horizonal rule.</p>
-
-        <section class="storybook-section">
-            <h2 class="has-text-weight-bold storybook-section__header">Heading</h2>
-            <header class="heading">
-              <span class="small">What is Stencila?</span>
-              <h1 class="title">An open source office suite for reproducible research.</h1>
-            </header>
-        </section>
-    </div>
-  `)
+## Options
+It contains an optional small text, h1-h6 tag & an optional horizonal rule.
+  `}})
