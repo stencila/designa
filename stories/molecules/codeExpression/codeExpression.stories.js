fbcc5e07f3f132626a871f69379a6e05b9b86080
Nokome Bentley <nokome@stenci.la>
2019-09-14 10:12:07 +1200

    test(CodeExpression): Add a story with for code expression within text

--- stories/molecules/codeExpression/codeExpression.stories.js
+++ stories/molecules/codeExpression/codeExpression.stories.js
@@ -1,14 +1,28 @@
 import { storiesOf } from '@storybook/html'
 
-storiesOf('Molecules/CodeExpression', module).add(
-  'CodeExpression',
-  () => `<div>
-    <stencila-codeexpression
-      data-collapsed="false"
-      itemtype="stencila:CodeChunk"
-    >
-      <pre slot="text"><code>x * y</code></pre>
-      <pre slot="output"><output>Reaaaaalllyyyyyyyy loooongggggggggggggggggggg output</output></pre>
-    </stencila-codeexpression>
-  `
-)
+storiesOf('Molecules/CodeExpression', module)
+  .add(
+    'CodeExpression',
+    () => `<div>
+      <stencila-codeexpression
+        data-collapsed="false"
+        itemtype="stencila:CodeChunk"
+      >
+        <pre slot="text"><code>x * y</code></pre>
+        <pre slot="output"><output>Reaaaaalllyyyyyyyy loooongggggggggggggggggggg output</output></pre>
+      </stencila-codeexpression>
+    `
+  )
+  .add(
+    'CodeExpression within a paragraph',
+    () => `<p>
+      This is a paragraph with a code expresssion inside it
+      <stencila-codeexpression
+        data-collapsed="false"
+        itemtype="stencila:CodeChunk"
+      >
+        <pre slot="text"><code>x * y</code></pre>
+        <pre slot="output"><output>42</output></pre>
+      </stencila-codeexpression> followed by some more text.
+    </p>`
+  )
