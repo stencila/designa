727e0d2894142ca507078ed3b06125af2ee09876
alex-ketch <alex@ketch.me>
2020-10-20 10:53:36 -0400

    fix(Toast): Allow instantiating Toast component from outside Designa pkg

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,4 @@
 import 'animate-presence'
 export type { Components, JSX } from './components'
 export type { IconNames } from './components/icon/iconNames'
+export * as Toast from './components/toast/toastController'
