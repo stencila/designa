743b8324bcf6e5500ec14310d3924ff16a0ce8de
alex-ketch <alex@ketch.me>
2020-11-19 22:26:03 -0500

    docs(Storybook): Remove usage of deprecated Knobs Addob

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
   // stories: ['../stories/**/*.stories.@(ts|js|mdx)'],
-  presets: ['@storybook/addon-docs/preset', '@storybook/addon-knobs/preset'],
+  presets: ['@storybook/addon-docs/preset'],
   addons: [
+    '@storybook/addon-controls',
     '@storybook/addon-a11y/register',
     '@storybook/addon-backgrounds/register',
     '@storybook/addon-options/register',
@@ -9,6 +10,5 @@ module.exports = {
     '@storybook/addon-viewport/register',
     '@storybook/addon-cssresources/register',
     'storybook-addon-root-attribute/register',
-    '@storybook/addon-controls',
   ],
 }
