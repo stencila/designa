b4c63e4dd15ebaedda0a2fd34740cd1e50270efe
alex-ketch <alex@ketch.me>
2021-05-31 22:51:44 -0400

    fix(Styles): Fix PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,26 @@
 const path = require('path')
+const postcss = require('postcss')
 
 module.exports = (ctx) => ({
   modules: false,
   map: ctx.options.map,
   plugins: [
-    require('postcss-mixins')({
-      mixinsDir: path.join(__dirname, 'utils', 'css', 'mixins'),
-    }),
     require('postcss-import'),
     require('postcss-import-url')({ modernBrowser: true }),
     require('postcss-url')({ url: 'rebase' }),
-    require('tailwindcss')(path.join(__dirname, 'tailwind.config.js')),
+    function (root) {
+      return postcss([
+        require('postcss-mixins')({
+          mixinsDir: path.join(__dirname, 'utils', 'css', 'mixins'),
+        }),
+      ]).process(root)
+    },
+    require('tailwindcss')({
+      config: path.join(__dirname, 'tailwind.config.js'),
+    }),
     require('postcss-nested'),
     require('postcss-custom-properties')(),
     require('postcss-host'),
-    require('cssnano')({
-      preset: ['advanced', { discardComments: false }],
-    }),
+    require('cssnano'),
   ],
 })
