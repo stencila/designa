69505b52114a70f966a870fdd3d16454161f1f81
alex-ketch <alex@ketch.me>
2021-07-06 16:23:35 -0400

    chore(Build): Use ParcelV2 for faster builds

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -5,13 +5,6 @@ import path from 'path'
 import tailwind from 'tailwindcss'
 import { generateJsonDocs } from './jsonDocTransformer'
 
-const tailwindConfigPath = path.join(
-  __dirname,
-  '..',
-  '..',
-  'tailwind.config.js'
-)
-
 const prodPlugins =
   process.env.NODE_ENV === 'production'
     ? [
@@ -52,7 +45,7 @@ export const config: Config = {
   ],
   plugins: [
     postcss({
-      plugins: [tailwind(tailwindConfigPath), ...prodPlugins],
+      plugins: [tailwind('./tailwind.config.js'), ...prodPlugins],
     }),
   ],
   testing: {
