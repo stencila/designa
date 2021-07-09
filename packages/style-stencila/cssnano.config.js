02748e1b9f52bb58b37fdab5ff6f5badfe0b2f44
alex-ketch <alex@ketch.me>
2021-07-09 16:39:36 -0400

    fixup! fixup! chore(Build): Use ParcelV2 for faster builds

---
+++ packages/style-stencila/cssnano.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: [
+    'advanced',
+    {
+      discardComments: {
+        // Don't discard important or WebComponent prop documentation
+        remove: (comment) => {
+          return !(comment.startsWith('!') || comment.includes('@prop'))
+        },
+      },
+    },
+  ],
+}
