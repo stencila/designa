cfe0d6ed49d41505fe281cc47dc9236786a62a59
alex-ketch <alex@ketch.me>
2021-07-23 09:15:56 -0400

    test(Toast): Fix test fixture to include new component props

--- packages/components/src/components/toast/toast.e2e.ts
+++ packages/components/src/components/toast/toast.e2e.ts
@@ -3,7 +3,7 @@ import { newE2EPage } from '@stencil/core/testing'
 describe('stencila-toast', () => {
   it('renders', async () => {
     const page = await newE2EPage()
-    await page.setContent('<stencila-toast></stencila-toast>')
+    await page.setContent('<stencila-toast duration="0"></stencila-toast>')
 
     const element = await page.find('stencila-toast')
     expect(element).toHaveClass('hydrated')
