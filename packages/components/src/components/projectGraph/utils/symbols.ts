4304573b5450cbe565382a5e0702f6db02ffac95
alex-ketch <alex@ketch.me>
2021-08-23 15:54:38 -0400

    Update styles and colour scheme

--- packages/components/src/components/projectGraph/utils/symbols.ts
+++ packages/components/src/components/projectGraph/utils/symbols.ts
@@ -1,13 +1,34 @@
-import { symbol, symbolCircle, symbolSquare } from 'd3-shape'
+import {
+  symbol,
+  symbolCircle,
+  symbolDiamond,
+  symbolSquare,
+  symbolStar,
+  symbolTriangle,
+  symbolWye,
+} from 'd3-shape'
 import { Resource } from '../types'
 
-export const getResourceSymbol = (resource: Resource) => {
+export const getResourceSymbol = (resource: Resource, size = 512) => {
   switch (resource.type) {
-    case 'Url': {
-      return symbol(symbolSquare, 24)
+    case 'Node': {
+      return symbol(symbolTriangle, size)
     }
+    case 'Symbol': {
+      return symbol(symbolDiamond, size)
+    }
+    case 'Source': {
+      return symbol(symbolStar, size)
+    }
+    case 'File': {
+      return symbol(symbolWye, size)
+    }
+    case 'Url': {
+      return symbol(symbolSquare, size)
+    }
+    case 'Module':
     default: {
-      return symbol(symbolCircle, 24)
+      return symbol(symbolCircle, size)
     }
   }
 }
